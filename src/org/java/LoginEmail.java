1dd5ce87aed2a03b86cdd9b5e79511b5304ad6f4
maheshwari1026 <146749774+maheshwari1026@users.noreply.github.com>
2023-11-17 15:11:15 +0530

    mohana few lines added

--- src/org/java/LoginEmail.java
+++ src/org/java/LoginEmail.java
@@ -8,9 +8,13 @@ public class LoginEmail {
     private void failed() {
  System.out.println("tested case : 5");
 	}
+    private void passed() {
+ System.out.println("test case : 6");
+	}
 public static void main(String[] args) {
 	LoginEmail d= new LoginEmail();
 	d.textbox();
 	d.failed();
+	d.passed();
 }
 }
