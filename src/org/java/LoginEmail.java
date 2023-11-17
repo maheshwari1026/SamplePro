d8f7afc4fd033bdc5da75ef80920aa1e025e4347
maheshwari1026 <146749774+maheshwari1026@users.noreply.github.com>
2023-11-17 12:14:34 +0530

    add few lines

---
+++ src/org/java/LoginEmail.java
@@ -0,0 +1,12 @@
+package org.java;
+
+public class LoginEmail {
+	private void textbox() {
+		System.out.println("Text");
+        System.out.println("passed");
+	}
+public static void main(String[] args) {
+	LoginEmail d= new LoginEmail();
+	d.textbox();
+}
+}
